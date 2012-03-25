47b73e6e4039320e54c4b390ad13847b5bda2134
Qba S <agatomousike@gmail.com>
2012-03-25 22:24:36 +0200

    model_plansza

--- evvie/model/Klocek.java
+++ evvie/model/Klocek.java
@@ -1,7 +1,38 @@
 package evvie.model;
 
 public class Klocek {
-	LiczbaSegementow liczbaSegmentow;
-	OrientacjaKlocka orientacja;
+	final LiczbaSegementow liczbaSegmentow;
+	final OrientacjaKlocka orientacja;
+	PozycjaNaPlanszy pozycjaNaPlanszy;
 	
+	public Klocek(LiczbaSegementow liczbaSegmentow,
+			OrientacjaKlocka orientacja, PozycjaNaPlanszy pozycjaNaPlanszy) {
+		super();
+		this.liczbaSegmentow = liczbaSegmentow;
+		this.orientacja = orientacja;
+		this.pozycjaNaPlanszy = pozycjaNaPlanszy;
+	}
+
+	public Klocek(LiczbaSegementow liczbaSegmentow, OrientacjaKlocka orientacja) {
+		super();
+		this.liczbaSegmentow = liczbaSegmentow;
+		this.orientacja = orientacja;
+		this.pozycjaNaPlanszy = PozycjaNaPlanszy.P0;
+	}
+
+	public PozycjaNaPlanszy getPozycjaNaPlanszy() {
+		return pozycjaNaPlanszy;
+	}
+
+	public void setPozycjaNaPlanszy(PozycjaNaPlanszy pozycjaNaPlanszy) {
+		this.pozycjaNaPlanszy = pozycjaNaPlanszy;
+	}
+
+	public LiczbaSegementow getLiczbaSegmentow() {
+		return liczbaSegmentow;
+	}
+
+	public OrientacjaKlocka getOrientacja() {
+		return orientacja;
+	}
 }
---
+++ evvie/model/Plansza.java
@@ -0,0 +1,9 @@
+package evvie.model;
+
+public class Plansza {
+	String idPlanszy;
+	String opisPlanszy;
+	PolePlanszy [] plansza; /*czy pole jest zajête czy wolne i jaki klocek siê tam znajduje*/
+	Klocek[] klocki; /*zbiór klocków na planszy*/
+	
+}
